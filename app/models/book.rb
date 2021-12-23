e390bb110dd3abe76ac343806cf86799c9d77697
itsuki.kikuyama <tf.k.itsuki.rits@gmail.com>
2021-12-23 15:09:11 +0900

    モック、スタブの使い方

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,13 @@ class Book < ApplicationRecord
   def title_with_author
     "#{title} - #{author}"
   end
+
+  def bonus
+    return "著者サイン入りチェキ" if lucky?
+    "しおり"
+  end
+
+  def lucky?
+    [true, false].sample
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -45,4 +45,15 @@ RSpec.describe Book, type: :model do
       end
     end
   end
+
+  describe "Book#bonus" do
+    context "lucky?がtrueのとき" do
+      it "チェキが返ること" do
+        book = Book.new
+        allow(book).to receive(:lucky?).and_return(true)
+        expect(book).to receive(:lucky?)
+        expect(book.bonus).to eq("著者サイン入りチェキ")
+      end
+    end
+  end
 end
