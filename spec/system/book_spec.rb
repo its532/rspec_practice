0f639b1854168f8c8f717389efe32d16798512e9
itsuki.kikuyama <tf.k.itsuki.rits@gmail.com>
2021-12-23 11:21:48 +0900

    postのシステムテスト

--- spec/system/book_spec.rb
+++ spec/system/book_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe "books", type: :system do
     visit "/books"
     expect(page).to have_text("Books")
   end
+
+  it "POST /books" do
+    visit "/books/new"
+    fill_in "Title", with: "RubyBook"
+    fill_in "Author", with: "its"
+    click_button "Create Book"
+
+    expect(page).to have_text("Book was successfully created.")
+    expect(page).to have_text("Title: RubyBook")
+    expect(page).to have_text("Author: its")
+  end
 end
